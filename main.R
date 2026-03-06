c06e82129197feeaa295be4de0c776894a1c26a4
fabradimitra <fabriziodimari@gmail.com>
2026-03-06 11:25:34 +0100

    Managed to perform regression splines on growth

--- main.R
+++ main.R
@@ -3,16 +3,32 @@ library(splines2)
 data(growth)
 with(growth, matplot(age, hgtf[, 1:10], type="b"))
 
-argvals <- seq.int(0.75, 18.25, .25)
-nsbase <- naturalSpline(x = argvals, knots = growth$age)
-matplot(nsbase, ty="l")
+x_obs  <- growth$age
+iknots <- growth$age[-c(1, length(growth$age))]
+bs_obs  <- naturalSpline(
+        x = x_obs, 
+        knots = iknots, derivs = 0)   # 31 x 30
 
-Y<- t(growth$hgtm)
+Y      <- t(growth$hgtm)                                     # 39 x 31
 
-c    <- solve(crossprod(nsbase)) %*% crossprod(nsbase, t(Y))
-yhat <- nsbase %*% c
+coef_basis <- solve(crossprod(bs_obs))%*%crossprod(bs_obs, t(Y))  # 32 x 39
 
-matplot(argvals, Y[1,], ty="l", lty=1, col = "lightgrey", ylim = c(0.35, 0.77),
-        xlab = "Location within corpus collosum",
-        ylab = "Fractional Anisotropy")
-matlines(argvals, yhat[,1], col = "darkred")
+x_grid <- seq.int(1, 18, .05)
+bs_grid <- naturalSpline(
+        x = x_grid, 
+        knots = iknots,
+        Boundary.knots = range(x_obs))   # 73 x 32
+
+matplot(
+  x_grid, bs_grid,
+  type = "l", lty = 1, lwd = 2,
+  xlab = "Age", ylab = "Basis value",
+  main = "Natural Spline Basis Functions")
+
+yhat   <- bs_grid %*% coef_basis
+
+matplot(x_obs, Y[1,], ty="b", lty=1, col = "grey",
+        xlab = "Age",
+        ylab = "Height (cm)")
+matlines(x_grid, yhat[,1], col = "darkred")
+
